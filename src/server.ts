404188202b310fc5cbbc20c0affb2dfee2668b12
KarimAbdelnasser <kareemabdo38@yahoo.com>
2022-12-07 15:33:44 +0200

    add prettier and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,77 @@
-import express from "express";
-import { promises as fs } from "fs";
-import sharp from "sharp";
-import NodeCache from "node-cache";
+import express from 'express';
+import { promises as fs } from 'fs';
+import sharp from 'sharp';
+import NodeCache from 'node-cache';
 
 const cache = new NodeCache();
 const PORT = 8080;
 const app = express();
 
 interface myQuery {
-    filename?: string;
+  filename?: string;
 }
 
 //middleware
 const verifyCache = (
-    req: express.Request,
-    res: express.Response,
-    next: Function
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
 ) => {
-    try {
-        const { filename } = req.query as myQuery;
-        const chosenWidth: number = Number(req.query.width);
-        const chosenHeight: number = Number(req.query.height);
-        const name: string = `${filename}${chosenWidth}${chosenHeight}`;
-        if (cache.has(name)) {
-            console.log(`cached image with name ${name} has been found`);
-            return res
-                .status(200)
-                .setHeader("Content-Type", "image/jpg")
-                .send(cache.get(name));
-        }
-        console.log(
-            `Could not find an image with name ${name} in cached images!`
-        );
-        return next();
-    } catch (err) {
-        console.log(err);
+  try {
+    const { filename } = req.query as myQuery;
+    const chosenWidth = Number(req.query.width);
+    const chosenHeight = Number(req.query.height);
+    const name = `${filename}${chosenWidth}${chosenHeight}`;
+    if (cache.has(name)) {
+      console.log(`cached image with name ${name} has been found`);
+      return res
+        .status(200)
+        .setHeader('Content-Type', 'image/jpg')
+        .send(cache.get(name));
     }
+    console.log(`Could not find an image with name ${name} in cached images!`);
+    return next();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //image route
-app.get("/img", verifyCache, async (req, res) => {
+app.get(
+  '/img',
+  verifyCache,
+  async (req: express.Request, res: express.Response) => {
     const { filename } = req.query as myQuery;
-    const chosenWidth: number = Number(req.query.width);
-    const chosenHeight: number = Number(req.query.height);
+    const chosenWidth = Number(req.query.width);
+    const chosenHeight = Number(req.query.height);
     if (!filename) {
-        return res
-            .status(400)
-            .send(
-                `Could not find an image with the given file name ${filename}!`
-            );
+      return res
+        .status(400)
+        .send(`Could not find an image with the given file name ${filename}!`);
     } else if (!chosenWidth || !chosenHeight) {
-        console.log(`No width or height is given, send original image!`);
-        return res
-            .status(200)
-            .setHeader("Content-Type", "image/jpg")
-            .sendFile(__dirname + `/images/${filename}.jpg`);
+      console.log(`No width or height is given, send original image!`);
+      return res
+        .status(200)
+        .setHeader('Content-Type', 'image/jpg')
+        .sendFile(__dirname + `/images/${filename}.jpg`);
     }
-    let myName: string = `${filename}${chosenWidth}${chosenHeight}`;
-    let imgPath = __dirname + `/images/${filename}.jpg`;
-    let newImgPath = `${__dirname}/thumb/${filename}${chosenWidth}${chosenHeight}_thumb.jpg`;
+    const cacheKey = `${filename}${chosenWidth}${chosenHeight}`;
+    const imgPath = __dirname + `/images/${filename}.jpg`;
+    const newImgPath = `${__dirname}/thumb/${filename}${chosenWidth}${chosenHeight}_thumb.jpg`;
     await sharp(imgPath)
-        .resize({
-            width: chosenWidth,
-            height: chosenHeight,
-        })
-        .toFile(newImgPath);
+      .resize({
+        width: chosenWidth,
+        height: chosenHeight,
+      })
+      .toFile(newImgPath);
     const data = await fs.readFile(newImgPath);
-    cache.set(myName, data);
-    res.status(201).setHeader("Content-Type", "image/jpg").sendFile(newImgPath);
-});
+    cache.set(cacheKey, data);
+    res.status(201).setHeader('Content-Type', 'image/jpg').sendFile(newImgPath);
+  }
+);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+  console.log(`Server is running on port ${PORT}...`);
 });
 
 export default app;
--- src/tests/imgSpec.ts
+++ src/tests/imgSpec.ts
@@ -1,34 +1,34 @@
-import app from "../server";
-import supertest from "supertest";
+import app from '../server';
+import supertest from 'supertest';
 const request = supertest(app);
 
-describe("Test endpoint responses", () => {
-    it("Should return a status code 200", async () => {
-        const response = await request.get("/img").query({
-            filename: "icelandwaterfall",
-        });
-        expect(response.status).toBe(200);
+describe('Test endpoint responses', () => {
+  it('Should return a status code 200', async () => {
+    const response = await request.get('/img').query({
+      filename: 'icelandwaterfall',
     });
-    it("Should return a status code 201", async () => {
-        const response = await request.get("/img").query({
-            filename: "icelandwaterfall",
-            width: 200,
-            height: 200,
-        });
-        expect(response.status).toBe(201);
+    expect(response.status).toBe(200);
+  });
+  it('Should return a status code 201', async () => {
+    const response = await request.get('/img').query({
+      filename: 'icelandwaterfall',
+      width: 200,
+      height: 200,
     });
-    it("Should return a status code 400", async () => {
-        const response = await request.get("/img");
-        expect(response.status).toBe(400);
-    });
-    it("Should return true", async () => {
-        await request
-            .get("/img")
-            .query({
-                filename: "icelandwaterfall",
-                width: 300,
-                height: 300,
-            })
-            .expect("Content-Type", /image/);
-    });
+    expect(response.status).toBe(201);
+  });
+  it('Should return a status code 400', async () => {
+    const response = await request.get('/img');
+    expect(response.status).toBe(400);
+  });
+  it('Should return true', async () => {
+    await request
+      .get('/img')
+      .query({
+        filename: 'icelandwaterfall',
+        width: 300,
+        height: 300,
+      })
+      .expect('Content-Type', /image/);
+  });
 });
